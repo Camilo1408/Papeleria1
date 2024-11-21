e181b5a524b9c44ad9478ce4c0a104393c2134f0
Alvarito <juliandelgado2201@gmail.com>
2024-11-21 10:18:15 -0500

    se crearon mas productos

--- src/assets/mockData.jsx
+++ src/assets/mockData.jsx
@@ -15,71 +15,245 @@ export const mockData = [
     {
         id:1,
         image: Image9,
-        name: "Producto 1",
+        name: "Bolígrafo InkJoy Gel Pen",
         price: 29.99,
-        category: "Colores"
+        category: "PaperMate",
+        description: "Bolígrafo de tinta gel que se seca rápidamente, ideal para escritura fluida y sin manchas."
 
     },
     {
         id:2,
         image: Image8,
-        name: "Producto 2",
+        name: "Bolígrafo Profile Retractable Ballpoint",
         price: 39.99,
-        category: "Colores"
+        category: "PaperMate",
+        description: "Bolígrafo retráctil con tinta suave y colores vibrantes."
     },
     {
         id:3,
         image: Image7,
-        name: "Producto 3",
+        name: "Lápices mecánicos Clearpoint",
         price: 39.99,
-        category: "Marcadores"
+        category: "PaperMate",
+        description: "Lápices mecánicos ergonómicos con borrador grande, ideales para estudiantes."
     },
     {
         id:4,
         image: Image6,
-        name: "Producto 4",
+        name: "Corrector líquido Liquid Paper",
         price: 39.99,
-        category: "Marcadores"
+        category: "PaperMate",
+        description: "Corrector blanco de secado rápido para cubrir errores en papel."
     },
     {
         id:5,
         image: Image10,
-        name: "Producto 5",
+        name: "Marcadores Flair Felt Tip Pens",
         price: 49.99,
-        category: "Cuadernos"
+        category: "PaperMate",
+        description: "Marcadores de punta de fieltro que no traspasan el papel, perfectos para escritura creativa."
     },
     {
         id:6,
         image: Image4,
-        name: "Producto 6",
+        name: "Bolígrafo Cristal Ballpoint Pen",
         price: 49.99,
-        category: "Cuadernos"
+        category: "BIC",
+        description: "Bolígrafo económico y confiable con tinta de larga duración."
     },
     {
         id:7,
         image: Image5,
-        name: "Producto 7",
+        name: "Bolígrafo Gel-ocity Quick Dry Gel Pen",
         price: 49.99,
-        category: "Utiles escolares"
+        category: "BIC",
+        description: " Bolígrafo de gel de secado rápido para una escritura suave y sin manchas."
     },
     {
         id:8,
         image: Image3,
-        name: "Producto 8",
+        name: "Encendedor BIC Classic Lighter (accesorio popular de la marca)",
         price: 49.99,
+        category: "BIC",
+        description: " Encendedor duradero, compacto y confiable."
     },
     {
         id:9,
         image: Image2,
-        name: "Producto 9",
+        name: "Lápiz mecánico BIC Xtra-Sparkle",
         price: 49.99,
+        category: "BIC",
+        description: " Lápices mecánicos con diseños coloridos y minas resistentes."
     },
     {
         id:10,
         image: Image1,
-        name: "Producto 10",
+        name: "Marcadores permanentes Intensity",
         price: 49.99,
+        category: "BIC",
+        description: " Marcadores de colores intensos para superficies múltiples."
     },
+    {
+        id:11,
+        image: Image1,
+        name: "Marcadores permanentes Intensity",
+        price: 49.99,
+        category: "BIC",
+        description: " Marcadores de colores intensos para superficies múltiples."
+    },
+    {
+        id:12,
+        image: Image1,
+        name: "Marcadores permanentes Intensity",
+        price: 49.99,
+        category: "BIC",
+        description: " Marcadores de colores intensos para superficies múltiples."
+    },
+    {
+        id:13,
+        image: Image1,
+        name: "Marcadores permanentes Intensity",
+        price: 49.99,
+        category: "BIC",
+        description: " Marcadores de colores intensos para superficies múltiples."
+    },
+    {
+        id:14,
+        image: Image1,
+        name: "Marcadores permanentes Intensity",
+        price: 49.99,
+        category: "BIC",
+        description: " Marcadores de colores intensos para superficies múltiples."
+    },
+    {
+        id:15,
+        image: Image1,
+        name: "Marcadores permanentes Intensity",
+        price: 49.99,
+        category: "BIC",
+        description: " Marcadores de colores intensos para superficies múltiples."
+    },
+    {
+        id:16,
+        image: Image1,
+        name: "Marcadores permanentes Intensity",
+        price: 49.99,
+        category: "BIC",
+        description: " Marcadores de colores intensos para superficies múltiples."
+    },
+    {
+        id:17,
+        image: Image1,
+        name: "Marcadores permanentes Intensity",
+        price: 49.99,
+        category: "BIC",
+        description: " Marcadores de colores intensos para superficies múltiples."
+    },
+    {
+        id:18,
+        image: Image1,
+        name: "Marcadores permanentes Intensity",
+        price: 49.99,
+        category: "BIC",
+        description: " Marcadores de colores intensos para superficies múltiples."
+    },
+    {
+        id:19,
+        image: Image1,
+        name: "Marcadores permanentes Intensity",
+        price: 49.99,
+        category: "BIC",
+        description: " Marcadores de colores intensos para superficies múltiples."
+    },
+    {
+        id:20,
+        image: Image1,
+        name: "Marcadores permanentes Intensity",
+        price: 49.99,
+        category: "BIC",
+        description: " Marcadores de colores intensos para superficies múltiples."
+    },
+    {
+        id:21,
+        image: Image1,
+        name: "Marcadores permanentes Intensity",
+        price: 49.99,
+        category: "BIC",
+        description: " Marcadores de colores intensos para superficies múltiples."
+    },
+    {
+        id:22,
+        image: Image1,
+        name: "Marcadores permanentes Intensity",
+        price: 49.99,
+        category: "BIC",
+        description: " Marcadores de colores intensos para superficies múltiples."
+    },
+    {
+        id:23,
+        image: Image1,
+        name: "Marcadores permanentes Intensity",
+        price: 49.99,
+        category: "BIC",
+        description: " Marcadores de colores intensos para superficies múltiples."
+    },
+    {
+        id:24,
+        image: Image1,
+        name: "Marcadores permanentes Intensity",
+        price: 49.99,
+        category: "BIC",
+        description: " Marcadores de colores intensos para superficies múltiples."
+    },
+    {
+        id:25,
+        image: Image1,
+        name: "Marcadores permanentes Intensity",
+        price: 49.99,
+        category: "BIC",
+        description: " Marcadores de colores intensos para superficies múltiples."
+    },
+    {
+        id:26,
+        image: Image1,
+        name: "Marcadores permanentes Intensity",
+        price: 49.99,
+        category: "BIC",
+        description: " Marcadores de colores intensos para superficies múltiples."
+    },
+    {
+        id:27,
+        image: Image1,
+        name: "Marcadores permanentes Intensity",
+        price: 49.99,
+        category: "BIC",
+        description: " Marcadores de colores intensos para superficies múltiples."
+    },
+    {
+        id:28,
+        image: Image1,
+        name: "Marcadores permanentes Intensity",
+        price: 49.99,
+        category: "BIC",
+        description: " Marcadores de colores intensos para superficies múltiples."
+    },
+    {
+        id:29,
+        image: Image1,
+        name: "Marcadores permanentes Intensity",
+        price: 49.99,
+        category: "BIC",
+        description: " Marcadores de colores intensos para superficies múltiples."
+    },
+    {
+        id:30,
+        image: Image1,
+        name: "Marcadores permanentes Intensity",
+        price: 49.99,
+        category: "BIC",
+        description: " Marcadores de colores intensos para superficies múltiples."
+    },
+
 ];
 
 export default mockData;
