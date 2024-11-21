fc49e3a1e8056916c2de063e75e59986a48439e8
Cesar Camilo <cesarcamilohoyosdiaz@gmail.com>
2024-11-21 10:39:07 -0500

    Imagenes Tienda

--- src/assets/mockData.jsx
+++ src/assets/mockData.jsx
@@ -1,13 +1,13 @@
-import Image1 from "./Images/bag.jpg";
-import Image2 from "./Images/sandle.jpg";
-import Image3 from "./Images/watch.jpg";
-import Image4 from "./Images/cap.jpg";
-import Image5 from "./Images/boot1.jpg";
-import Image6 from "./Images/dove.jpg";
-import Image7 from "./Images/headphone.jpg";
-import Image8 from "./Images/robot.jpg";
-import Image9 from "./Images/wwatch.jpg";
-import Image10 from "./Images/shirt.jpg";
+import Image1 from "./Images/IntensityBic.webp";
+import Image2 from "./Images/LapizMecanicoBic.webp";
+import Image3 from "./Images/EncendedorBic.webp";
+import Image4 from "./Images/BicCristal.webp";
+import Image5 from "./Images/DryGel.webp";
+import Image6 from "./Images/LiquidPaper.webp";
+import Image7 from "./Images/ClearPoint.webp";
+import Image8 from "./Images/BoligrafoProfile.webp";
+import Image9 from "./Images/BoligrafoInkJoy.webp";
+import Image10 from "./Images/FLair.webp";
 
 
 
