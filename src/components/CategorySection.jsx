891031faa1ff887cc9b745216ef40616d49371ac
Jost2167 <johnstiven2000@gmail.com>
2024-11-21 08:48:54 -0500

    Carousel de marcas

--- src/components/CategorySection.jsx
+++ src/components/CategorySection.jsx
@@ -1,36 +1,64 @@
-import Norma from "../assets/Images/norma.png"
-import Sharpie from "../assets/Images/sharpie.jpg"
-import Cassio from "../assets/Images/casio.jpg"
+import { useState } from "react";
+import Slider from "react-slick";
+import Norma from "../assets/Images/norma.png";
+import Sharpie from "../assets/Images/sharpie.jpg";
+import Cassio from "../assets/Images/casio.jpg";
 
 const marcas = [
-    {
-        title: '',
-        imageUrl: Norma
-    },
-    {
-        title: '',
-        imageUrl: Sharpie,
-    },
-    {
-        title: '',
-        imageUrl: Cassio,
-    },
+  { title: '', imageUrl: Norma },
+  { title: '', imageUrl: Sharpie },
+  { title: '', imageUrl: Cassio },
+  // Puedes agregar más imágenes aquí
 ];
 
 const CategorySection = () => {
-    return (
-        <div className="container mx-auto grid grid-cols-1 sm:grid-cols-3 gap-6 ">
-            {marcas.map((marca,index)=>(
-                <div key={index} className="relative h-64 transform transition-transform duration-300 hover:scale-105 cursor-pointer">
-                    <img src={marca.imageUrl} alt="" className="w-full h-full object-cover rounded-lg shadow-md"/>
-                    <div className="absolute top-20 left-12">
-                        <p className="text-xl font-bold">{marca.title}</p>
-                        <p className="text-gray-600"></p>
-                    </div>
-                </div>
-            ))}
-        </div>
-    ); 
+  const [marcasList] = useState(marcas);
+
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 3, // Número de elementos visibles
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  };
+
+  return (
+    <div className="container mx-auto">
+      <h2 className="text-3xl font-semibold text-center text-gray-900 mb-8">Marcas</h2>
+      <Slider {...settings}>
+        {marcasList.map((marca, index) => (
+          <div key={index} className="relative h-64 transform transition-transform duration-300 hover:scale-105 cursor-pointer">
+            <img
+              src={marca.imageUrl}
+              alt={`Marca ${index}`}
+              className="w-full h-full object-cover rounded-lg shadow-md"
+            />
+            <div className="absolute top-20 left-12">
+              <p className="text-xl font-bold">{marca.title}</p>
+            </div>
+          </div>
+        ))}
+      </Slider>
+    </div>
+  );
 };
 
 export default CategorySection;
