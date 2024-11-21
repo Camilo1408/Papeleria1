e64ebc771c01b3f012eabb8bca2fe51f9f8e7d3f
Cesar Camilo <cesarcamilohoyosdiaz@gmail.com>
2024-11-21 11:54:04 -0500

    Height carrito y product details

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -17,7 +17,7 @@ const Cart = () => {
 
 
     return (
-        <div className="container mx-auto py-8 min-h-96 px-4 md:px-16 lg:px-24">
+        <div className="h-[90vh] container mx-auto py-8 min-h-96 px-4 md:px-16 lg:px-24">
             {cart.products.length > 0 ? (
                 <div>
                     <h3 className="text-2xl font-semibold mb-4">Carrito de compras</h3>
--- src/pages/ProductDetail.jsx
+++ src/pages/ProductDetail.jsx
@@ -46,7 +46,7 @@ const ProductDetail = () => {
     if(!product) return <div>Cargando ...</div>
 
     return(
-        <div className="container mx-auto py-8 px-4 md:px-16 lg:px-24">
+        <div className=" h-[90vh] container mx-auto py-8 px-4 md:px-16 lg:px-24">
             <div className="flex flex-col md:flex-row gap-x-16">
                 <div className="md:w-1/2 py-4 shadow-md md:px-8 h-96 flex justify-center">
                     <img src={product.image} alt={product.name} className="h-full"/>
